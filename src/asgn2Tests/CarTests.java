6ebf732f9af5e96fe4e1755881fc3c16004f131b
a98ccp <a98ccp@gmail.com>
2014-05-17 15:11:09 +1000

    First Version of Vehicle Tests please check if any test is missing

--- src/asgn2Tests/CarTests.java
+++ src/asgn2Tests/CarTests.java
@@ -1,63 +1,65 @@
 /**
  * 
  * This file is part of the CarParkSimulator Project, written as 
- * part of the assessment for INB370, semester 1, 2014. 
+ * part of the assessment for INB370, semester 1, 2014.
+ * This test will only test the constructor of 
+ * the class Car, since the other methods are
+ * inherited form abstract class vehicle directly,
+ * so they will be tested in MotorCycleTests.java
  *
  * CarParkSimulator
  * asgn2Tests 
- * 22/04/2014
+ * 15/05/2014
  * 
+ * @author Chun Pui Chan
  */
 package asgn2Tests;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-/**
- * @author hogan
- *
- */
+import asgn2Exceptions.*;
+import asgn2Vehicles.*;
+
 public class CarTests {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	/**
-	 * Test method for {@link asgn2Vehicles.Car#toString()}.
-	 */
-	@Test
-	public void testToString() {
-		fail("Not yet implemented"); // TODO
-	}
+	Car car;
+	
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Car#Car(java.lang.String, int, boolean)}.
+	 * @throws VehicleException 
+	 * 
+	 * ArrivalTimeZeroTest: set the arrival time of a car to zero,
+	 * expected to throw a VehicleException
+	 * 
+	 * ArrivalTimeNegativeTest: set arrival time of a car to a negative value,
+	 * expected to throw a VehicleException
 	 */
-	@Test
-	public void testCar() {
-		fail("Not yet implemented"); // TODO
+	@Test (expected = VehicleException.class)
+	public void CarConstructArrivalTimeZeroTest() throws VehicleException {
+		car = new Car("C1",0,false);
+	}
+	@Test (expected = VehicleException.class)
+	public void CarConstructArrivalTimeNegativeTest() throws VehicleException {
+		car = new Car("C1",-1,false);
 	}
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Car#isSmall()}.
+	 * 
+	 * isSmallTest: set a car to not to be small and check,
+	 * then set a car to be small and check.
 	 */
 	@Test
-	public void testIsSmall() {
-		fail("Not yet implemented"); // TODO
+	public void isSmallTest() throws VehicleException {
+		car = new Car("C1",10,false);
+		assertFalse("Fail to indicate a car is not small",car.isSmall());
+		car = new Car("C1", 10, true);
+		assertTrue("Fail to indicate a car is small",car.isSmall());
+		
+		
 	}
 
 }
--- src/asgn2Tests/MotorCycleTests.java
+++ src/asgn2Tests/MotorCycleTests.java
@@ -5,171 +5,377 @@
  *
  * CarParkSimulator
  * asgn2Tests 
- * 22/04/2014
+ * 15/05/2014
  * 
+ * 
+ * @author Chun Pui Chan
  */
 package asgn2Tests;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * @author hogan
- *
- */
-public class MotorCycleTests {
+import asgn2Exceptions.*;
+import asgn2Simulators.Constants;
+import asgn2Vehicles.*;
 
+public class MotorCycleTests {
+	
+	MotorCycle motorCycle;
+	
+	
 	/**
 	 * @throws java.lang.Exception
+	 * initial a motorCycle object
 	 */
 	@Before
 	public void setUp() throws Exception {
+		//MotorCycle(String vehID, int arrivalTime)
+		motorCycle = new MotorCycle("MC1",10);
 	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
+	
+	
 	/**
 	 * Test method for {@link asgn2Vehicles.MotorCycle#MotorCycle(java.lang.String, int)}.
-	 */
-	@Test
-	public void testMotorCycle() {
-		fail("Not yet implemented"); // TODO
+	 * 
+	 * ArrivalTimeZeroTest: set the arrival time of a vehicle to zero,
+	 * expected to throw a VehicleException
+	 * 
+	 * ArrivalTimeNegativeTest: set arrival time of a vehicle to a negative value,
+	 * expected to throw a VehicleException
+	 * 
+	 * @throws VehicleException
+	 */	
+	@Test(expected = VehicleException.class)
+	public void MotorCycleConstructArrivalTimeZeroTest() throws VehicleException {
+		motorCycle = new MotorCycle("MC1",0);
 	}
-
-	/**
-	 * Test method for {@link asgn2Vehicles.Vehicle#Vehicle(java.lang.String, int)}.
-	 */
-	@Test
-	public void testVehicle() {
-		fail("Not yet implemented"); // TODO
+	
+	@Test(expected = VehicleException.class)
+	public void MotorCycleConstructArrivalTimeNegativeTest() throws VehicleException {
+		motorCycle = new MotorCycle("MC1",-1);
 	}
+		
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#getVehID()}.
+	 * 
+	 * getVehIDTest: check if the vehID is set according to the constructor
 	 */
 	@Test
-	public void testGetVehID() {
-		fail("Not yet implemented"); // TODO
+	public void getVehIDTest() {
+		assertEquals("Fail to get correct vehID","MC1",motorCycle.getVehID());		
 	}
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#getArrivalTime()}.
+	 * 
+	 * getArrivalTimeTest: check if the arrival time is set as constructor
 	 */
 	@Test
-	public void testGetArrivalTime() {
-		fail("Not yet implemented"); // TODO
+	public void getArrivalTimeTest() {
+		assertEquals("Fail to get correct arrival time",10,motorCycle.getArrivalTime());
 	}
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#enterQueuedState()}.
+	 * 
+	 * enterQueuedStateTest: send the motorCycle to queue state
+	 * 						 and check if the vehicle is actually in queue state
+	 * 
+	 * enterQueuedStateVehicleAlreadyInQueuedTest: send the vehicle to queue state twice
+	 * @throws VehicleExceptions 
+	 * 
+	 * enterQueuedStateVehicleAlreadyInParkedTest: send the vehicle to park state and try to send it to queue state
 	 */
 	@Test
-	public void testEnterQueuedState() {
-		fail("Not yet implemented"); // TODO
+	public void enterQueuedStateTest() throws VehicleException {
+		motorCycle.enterQueuedState();
+		assertTrue("queue state is not set to queued", motorCycle.isQueued());
 	}
+	
+	
+	@Test(expected = VehicleException.class)
+	public void enterQueuedStateVehicleAlreadyInQueuedTest() throws VehicleException {
+		try {motorCycle.enterQueuedState();} 
+		catch (VehicleException e){ fail("MotorCycle cannot enter first queue state.");} //use try-catch to ensure the vehicleException expected is not throw by the 1st enterQueuedState
+		motorCycle.enterQueuedState();
+	}
+	
+	@Test(expected = VehicleException.class)
+	public void enterQueuedStateVehicleAlreadyInParkedTest() throws VehicleException {
+		try {motorCycle.enterParkedState(40, Constants.MINIMUM_STAY +1);} 
+		catch (VehicleException e){ fail("MotorCycle cannot enter park state.");} //use try-catch to ensure the vehicleException expected is not throw by enterParkedState
+		motorCycle.enterQueuedState();
+	}
+	
+	
+	
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#exitQueuedState(int)}.
+	 * 
+	 * exitQueuedStateToArchiveExitTimeCorrectTest: send the vehicle to queue and push it out the queue,
+	 * 												then check if the vehicle's departure time is correctly set
+	 * 
+	 * exitQueuedStateVehicleInParkedStateTest: push the vehicle out the queue while the vehicle is in park state
+	 * @throws VehicleException
+	 * 
+	 * exitQueuedStateVehicleInNewStateTest: push the vehicle out the queue while the vehicle is in new state
+	 * @throws VehicleException
+	 * 
+	 * exitQueuedStateVehicleInArchivedStateTest:push the vehicle out the queue while the vehicle is in archived state
+	 * @throws VehicleException
+	 * 
+	 * exitQueuedStateExitTimeEarlierThanArrivalTimeTest: push the vehicle out the queue with time earlier than entered time
+	 * @throws VehicleException
+	 * 
+	 * exitQueuedStateExitTimeEqualToArrivalTimeTest: push the vehicle out the queue with time equals entered time
+	 * @throws VehicleException
 	 */
-	@Test
-	public void testExitQueuedState() {
-		fail("Not yet implemented"); // TODO
+	@Test 
+	public void exitQueuedStateToArchiveExitTimeCorrectTest() throws VehicleException {		
+		//arrivalTime = 10
+		motorCycle.enterQueuedState();
+		motorCycle.exitQueuedState(60);
+		assertEquals("Fail to set correct archive departure time",60,motorCycle.getDepartureTime());
 	}
+	
+	@Test(expected = VehicleException.class)
+	public void exitQueuedStateVehicleInParkedStateTest() throws VehicleException {
+		try {motorCycle.enterParkedState(40, Constants.MINIMUM_STAY +1);} 
+		catch (VehicleException e){ fail("MotorCycle cannot enter park state.");} //use try-catch to ensure the vehicleException expected is not throw by enterParkedState
+		motorCycle.exitQueuedState(80);
+	}
+	
+	@Test(expected = VehicleException.class)
+	public void exitQueuedStateVehicleInNewStateTest() throws VehicleException {
+		motorCycle.exitQueuedState(80);
+	}
+	
+	@Test(expected = VehicleException.class)
+	public void exitQueuedStateVehicleInArchivedStateTest() throws VehicleException {
+		try {
+			motorCycle.enterQueuedState();
+			motorCycle.exitQueuedState(79);
+			} 
+		catch (VehicleException e){ fail("Park state cannot change to archived.");} //use try-catch to ensure the vehicleException expected is not throw by other methods
+		motorCycle.exitQueuedState(80);
+	}
+	
+	@Test(expected = VehicleException.class)
+	public void exitQueuedStateExitTimeEarlierThanArrivalTimeTest() throws VehicleException {
+		try {motorCycle.enterQueuedState();} 
+		catch (VehicleException e){ fail("MotorCycle cannot enter queue state.");} //use try-catch to ensure the vehicleException expected is not throw by enterQueuedState
+		motorCycle.exitQueuedState(9); //(9 = arrivalTime - 1)
+	}
+	
+	@Test(expected = VehicleException.class)
+	public void exitQueuedStateExitTimeEqualToArrivalTimeTest() throws VehicleException {
+		try {motorCycle.enterQueuedState();} 
+		catch (VehicleException e){ fail("MotorCycle cannot enter queue state.");} //use try-catch to ensure the vehicleException expected is not throw by enterQueuedState
+		motorCycle.exitQueuedState(10);
+	}
+	
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#enterParkedState(int, int)}.
+	 *
+	 * enterParkedStateTest: send the vehicle to park, then check if it is actually parked
+	 * 
+	 * enterParkedStateVehicleAlreadyInParkedStateTest: send the vehicle to park, twice
+	 * @throws VehicleException
+	 * 
+	 * enterParkedStateVehicleAlreadyInQueuedStateTest: send the vehicle to queue, then send it to park
+	 * @throws VehicleException
+	 * 
+	 * enterParkedStateParkingTimeZeroTest: set the parking time to be zero and check if it pass
+	 * 
+	 * enterParkedStateNegativeParkingTimeTest: set parking time to a negative value
+	 * @throws VehicleException
+	 * 
+	 * enterParkedStateIntendedDurationTooShortTest: set intendedDuration as the minimum duration allowed minus one
+	 * @throws VehicleException
+	 * 
 	 */
 	@Test
-	public void testEnterParkedState() {
-		fail("Not yet implemented"); // TODO
+	public void enterParkedStateTest() throws VehicleException {
+		motorCycle.enterParkedState(40, Constants.MINIMUM_STAY +1);
+		assertTrue("park state is not set to parked", motorCycle.isParked());
+	}	
+	
+	@Test(expected = VehicleException.class)
+	public void enterParkedStateVehicleAlreadyInParkedStateTest() throws VehicleException {
+		try {motorCycle.enterParkedState(40, Constants.MINIMUM_STAY +1);} 
+		catch (VehicleException e){ fail("MotorCycle cannot enter first park state.");} //use try-catch to ensure the vehicleException expected is not throw by first enterParkedState
+		motorCycle.enterParkedState(40, Constants.MINIMUM_STAY +1);
 	}
+	
+	@Test(expected = VehicleException.class)
+	public void enterParkedStateVehicleAlreadyInQueuedStateTest() throws VehicleException {
+		try {motorCycle.enterQueuedState();} 
+		catch (VehicleException e){ fail("MotorCycle cannot enter queue state.");} //use try-catch to ensure the vehicleException expected is not throw by enterQueuedState
+		motorCycle.enterParkedState(40, Constants.MINIMUM_STAY +1);
+	}
+	
+	@Test
+	public void enterParkedStateParkingTimeZeroTest() throws VehicleException {
+		//enterParkedState(int parkingTime, int intendedDuration)
+		motorCycle.enterParkedState(0, Constants.MINIMUM_STAY +1);
+		assertEquals("Parking Time should able to be zero.",0,motorCycle.getParkingTime());
+	}
+	
+	@Test(expected = VehicleException.class)
+	public void enterParkedStateNegativeParkingTimeTest() throws VehicleException {
+		motorCycle.enterParkedState(-1, Constants.MINIMUM_STAY +1);
+	}
+	
+	@Test(expected = VehicleException.class)
+	public void enterParkedStateIntendedDurationTooShortTest() throws VehicleException {
+		motorCycle.enterParkedState(30,Constants.MINIMUM_STAY -1);
+	}
+	
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#exitParkedState(int)}.
+	 * 
+	 * exitParkedStateEarlierThanIntendedTest: send the vehicle to park, then exit the vehicle by a time shorter than intendedDuration
+	 * 
+	 * exitParkedStateVehicleInNewStateTest: exit the vehicle while it's in 'new' state
+	 * @throws VehicleException
+	 * 
+	 * exitParkedStateVehicleInQueuedStateTest: exit the vehicle while it's in 'queued' state
+	 * @throws VehicleException
+	 * 
+	 * exitParkedStateVehicleInArchivedStateTest: exit the vehicle while it's in 'archived' state
+	 * @throws VehicleException
+	 * 
+	 * exitParkedStateDepartTimeEarlierThanParkingTimeTest: exit the vehicle with departure earlier than parking time assigned
+	 * @throws VehicleException
 	 */
 	@Test
-	public void testExitParkedStateInt() {
-		fail("Not yet implemented"); // TODO
+	public void exitParkedStateEarlierThanIntendedTest() throws VehicleException {		
+		motorCycle.enterParkedState(40, Constants.MINIMUM_STAY + 1);
+		motorCycle.exitParkedState(40 + Constants.MINIMUM_STAY );
+		assertEquals("Fail to set departure time that earlier than the intened stay",
+						40 + Constants.MINIMUM_STAY ,motorCycle.getDepartureTime());
 	}
-
-	/**
-	 * Test method for {@link asgn2Vehicles.Vehicle#exitParkedState()}.
-	 */
-	@Test
-	public void testExitParkedState() {
-		fail("Not yet implemented"); // TODO
+	
+	@Test(expected = VehicleException.class)
+	public void exitParkedStateVehicleInNewStateTest() throws VehicleException {
+		motorCycle.exitParkedState(80);
 	}
+	
+	@Test(expected = VehicleException.class)
+	public void exitParkedStateVehicleInQueuedStateTest() throws VehicleException {
+		try {motorCycle.enterQueuedState();}
+		catch (VehicleException e) { fail("MotorCycle cannot enter queue state"); }  //use try-catch to ensure the vehicleException expected is not throw by enterQueuedState
+		motorCycle.exitParkedState(80);
+	}
+	
+	@Test(expected = VehicleException.class)
+	public void exitParkedStateVehicleInArchivedStateTest() throws VehicleException {
+		try{
+			motorCycle.enterParkedState(40, Constants.MINIMUM_STAY );
+			motorCycle.exitParkedState(40 + Constants.MINIMUM_STAY );
+			}
+		catch (VehicleException e){ fail("MotorCycle cannot enter archive state.");}  //use try-catch to ensure the vehicleException expected is not throw by other methods
+		motorCycle.exitParkedState(40 + Constants.MINIMUM_STAY );
+	}
+	
+	@Test(expected = VehicleException.class)
+	public void exitParkedStateDepartTimeEarlierThanParkingTimeTest() throws VehicleException {
+		//enterParkedState(int parkingTime, int intendedDuration)
+		try {motorCycle.enterParkedState(50, Constants.MINIMUM_STAY );}
+		catch (VehicleException e){ fail("MotorCycle cannot enter park state");}  //use try-catch to ensure the vehicleException expected is not throw by enterParkedState
+		//exitParkedState(int departureTime)
+		motorCycle.exitParkedState(49);
+	}	
+	
+	
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#isParked()}.
+	 * @throws VehicleException 
 	 */
-	@Test
-	public void testIsParked() {
-		fail("Not yet implemented"); // TODO
+	@Test 
+	public void isParkedTest() throws VehicleException {		
+		motorCycle.enterParkedState(40, Constants.MINIMUM_STAY +1);
+		assertTrue("Fail to show the vehicle is parked or not",motorCycle.isParked());
 	}
+		
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#isQueued()}.
+	 * isQueuedTest: send the vehicle to queued state and access this method
 	 */
-	@Test
-	public void testIsQueued() {
-		fail("Not yet implemented"); // TODO
+	@Test 
+	public void isQueuedTest() throws VehicleException {
+		motorCycle.enterQueuedState();
+		assertTrue("Fail to show the vehicle is queued or not", motorCycle.isQueued());
 	}
 
+		
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#getParkingTime()}.
+	 * getParkingTimeTest: send the vehicle to park and check the parking time with the assigned one
 	 */
 	@Test
-	public void testGetParkingTime() {
-		fail("Not yet implemented"); // TODO
+	public void getParkingTimeTest() throws VehicleException {
+		//enterParkedState(int parkingTime, int intendedDuration)
+		motorCycle.enterParkedState(30, Constants.MINIMUM_STAY );
+		assertEquals("Fail to get correct parking time",30,motorCycle.getParkingTime());	
 	}
 
+	
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#getDepartureTime()}.
+	 * getDepartureTimeTest: send the vehicle to park,
+	 * 						 check the estimated departure time (parking time + intendedDuration),
+	 * 						 exit the vehicle,
+	 * 						 check the actual departure time
 	 */
 	@Test
-	public void testGetDepartureTime() {
-		fail("Not yet implemented"); // TODO
+	public void getDepartureTimeTest() throws VehicleException {
+		motorCycle.enterParkedState(30, Constants.MINIMUM_STAY );
+		assertEquals("Fail to get correct departure time (estimated)",30 + Constants.MINIMUM_STAY ,motorCycle.getDepartureTime());
+		motorCycle.exitParkedState(30 + Constants.MINIMUM_STAY -1);
+		assertEquals("Fail to get correct departure time (actual)",30 + Constants.MINIMUM_STAY -1 ,motorCycle.getDepartureTime());
 	}
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#wasQueued()}.
+	 * wasQueuedTest: send the vehicle to queue, then check if the flag is changed
 	 */
 	@Test
-	public void testWasQueued() {
-		fail("Not yet implemented"); // TODO
+	public void wasQueuedTest() throws VehicleException {
+		assertFalse("Fail to show the vehicle was not queued", motorCycle.wasQueued());
+		motorCycle.enterQueuedState();
+		assertTrue("Fail to show the vehicle was queued", motorCycle.wasQueued());
 	}
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#wasParked()}.
+	 * wasParkedTest: send the vehicle to park, then check if the flag is changed
 	 */
 	@Test
-	public void testWasParked() {
-		fail("Not yet implemented"); // TODO
+	public void wasParkedTest() throws VehicleException {
+		assertFalse("Fail to determine when the vehicle was not parked", motorCycle.wasParked());
+		motorCycle.enterParkedState(30, Constants.MINIMUM_STAY ) ;
+		assertTrue("Fail to determine when the vehicle was parked", motorCycle.wasParked());
 	}
 
 	/**
 	 * Test method for {@link asgn2Vehicles.Vehicle#isSatisfied()}.
 	 */
 	@Test
-	public void testIsSatisfied() {
-		fail("Not yet implemented"); // TODO
+	public void isSatisfiedTest() throws VehicleException {		
+		assertFalse("Fail to determine when the customer was dissatisfied", motorCycle.wasParked());
+		motorCycle.enterParkedState(30, Constants.MINIMUM_STAY ) ;
+		assertTrue("Fail to determine when the customer was satisfied", motorCycle.wasParked());
 	}
-
-	/**
-	 * Test method for {@link asgn2Vehicles.Vehicle#toString()}.
-	 */
-	@Test
-	public void testToString() {
-		fail("Not yet implemented"); // TODO
-	}
-
+	
 }
